da8f68ba7de3d207eded71d59164a1d8ff70ad35
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2025-03-23 01:30:44 +0700

    modifi getall

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createNote, getNotes, getNoteById, updateNote, deleteNote } from "../controllers/noteController.js";
+import { createNote, getNotes, getNoteById, updateNote, deleteNote,getAllNotes } from "../controllers/noteController.js";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createNote);
+router.get('/dapat',  getAllNotes);
 router.get('/dapatsemua', verifyToken, getNotes);
 router.get('/dapat/:id', verifyToken, getNoteById);
 router.put('/update/:id', verifyToken, updateNote);
